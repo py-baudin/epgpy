e33563225f7154df8e017b4319309922e9450453
Pierre-Yves Baudin <py-baudin@institut-myologie.org>
2024-07-11 15:54:11 +0200

    fix MRF sequence

---
+++ TODO.md
@@ -0,0 +1,18 @@
+# TODO
+
+## examples
+- random gradient
+- other examples from PDG article
+
+## optimization
+- multiprocessed simulate (esp. with Jacobian/Hessian operators)
+
+## Squeeze
+- simple squeeze (scalar and matrix operators)
+- vectorized squeeze 
+- squeeze shift operator
+- squeeze 3d gradient operator
+
+## differentiation
+- check optim MRF example
+- compare with pruning option
--- examples/differentiation/optim_mrf.py
+++ examples/differentiation/optim_mrf.py
@@ -19,46 +19,57 @@ from epgpy import epg, stats, diff
 
 # define MRF sequence
 nTR = 400
+TE = 3 # ms
 T1, T2 = 1380, 80
+phi = 90
 
 # operators
 adc = epg.ADC
 grd = epg.S(1)
-inv = epg.T(180, 0) # inversion
+inv = epg.T(180, phi) # inversion
 rlx0 = epg.E(20, T1, T2, order1=['T1', 'T2'], duration=True) # inversion delay
+
 
 # parameters to optimize
 alphas = [f'alpha_{i:03d}' for i in range(nTR)]
-taus = [f'tau_{i:03d}' for i in range(nTR)]
+TRs = [f'TR_{i:03d}' for i in range(nTR)]
 
 # cross derivatives to compute in RF operator
-order2_rf = [[('T1', alphas[i]), ('T2', alphas[i])] for i in range(nTR)]
+order2_rf1 = [('T1', alphas[i]) for i in range(nTR)]
+order2_rf2 = [('T2', alphas[i]) for i in range(nTR)]
 
-def rf(i, alpha, deriv=False):
-    if not deriv:
-        return epg.T(alpha, (i%2) * 180)
+def rf(i, alpha, jac=False):
+    if not jac:
+        return epg.T(alpha, phi)
     return epg.T(
-        alpha, (i%2) * 180, 
+        alpha, phi, 
         order1={alphas[i]: 'alpha'}, # use parameter alias
-        order2=order2_rf[i], # select cross derivatives
+        order2=[order2_rf1[i], order2_rf2[i]], # select cross derivatives
         )
     
 # cross derivatives to compute in relaxation operator
-order2_rlx = [[('T1', taus[i]), ('T2', taus[i])] for i in range(nTR)]
-def rlx(i, tau, deriv=False): 
-    if not deriv:
-        return epg.E(tau, T1, T2, order1=['T1', 'T2'], duration=True)
+order2_rlx = [('T1', TRs[i]) for i in range(nTR)] + [('T2', TRs[i]) for i in range(nTR)]
+order2_rlx += order2_rf1 + order2_rf2
+
+rlx1_nojac = epg.E(TE, T1, T2, order1=['T1', 'T2'], duration=True)
+rlx1_jac = epg.E(TE, T1, T2, order1=['T1', 'T2'],  order2=order2_rlx, duration=True)
+def rlx1(jac=False): 
+    return rlx1_nojac if not jac else rlx1_jac
+
+def rlx2(i, TR, jac=False): 
+    if not jac:
+        return epg.E(TR - TE, T1, T2, order1=['T1', 'T2'], duration=True)
     return epg.E(
-        tau, T1, T2, 
-        order1={'T1': 'T1', 'T2': 'T2', taus[i]: 'tau'}, # use parameter aliases
-        order2=sum(order2_rlx + order2_rf, start=[]), # select cross derivatives
+        TR - TE, T1, T2, 
+        order1={'T1': 'T1', 'T2': 'T2', TRs[i]: 'tau'}, # use parameter aliases
+        order2=order2_rlx, # select cross derivatives
         duration=True,
     )
 
 # sequence generator function
-def sequence(angles, times, deriv=False):
+def sequence(angles, times, jac=False):
     return [inv, rlx0] + [
-        [rf(i, angles[i], deriv), rlx(i, times[i], deriv), adc, grd] 
+        [rf(i, angles[i], jac), rlx1(jac), adc, rlx2(i, times[i], jac), grd] 
         for i in range(nTR)
     ]
 
@@ -80,7 +91,8 @@ nstate = 10
 weights = [1, 1 / T1 ** 2, 1 / T2 ** 2]
 # Probe operators for Jacobien and Hessian
 Jac = epg.Jacobian(['magnitude', 'T1', 'T2'])
-Hes = epg.Hessian(['magnitude', 'T1', 'T2'], alphas + taus)
+Hes = epg.Hessian(['magnitude', 'T1', 'T2'], alphas + TRs)
+sigma2 = 1e1
 
 def Num(sm):
     # return number of computed partials derivatives
@@ -89,7 +101,7 @@ def Num(sm):
 # def condition(sm, th=1e-5):
 #     return sm.arrays.apply('states', lambda states: np.abs(states).max() < th)
 # remove phase states with little signal
-pruner = diff.PartialsPruner(condition=1e-6, variables=alphas + taus)
+pruner = diff.PartialsPruner(condition=1e-6, variables=alphas + TRs)
 
 # store parameters at each iteration
 iterations = []
@@ -100,24 +112,24 @@ def callback(params):
     iterations.append(params)
 
 def signal(params):
-    alphas, taus = params[:nTR], params[nTR:]
-    return epg.simulate(sequence(alphas, taus), max_nstate=nstate)
+    alphas, TRs = params[:nTR], params[nTR:]
+    return epg.simulate(sequence(alphas, TRs), max_nstate=nstate)
 
 
 def costfun(params):
     """crlb cost function"""
-    alphas, taus = params[:nTR], params[nTR:]
-    jac = epg.simulate(sequence(alphas, taus), probe=Jac, max_nstate=nstate)
-    cost = stats.crlb(np.moveaxis(jac, -1, 0), W=weights, log=True)
+    alphas, TRs = params[:nTR], params[nTR:]
+    jac = epg.simulate(sequence(alphas, TRs), probe=Jac, max_nstate=nstate)
+    cost = stats.crlb(np.moveaxis(jac, -1, 0), W=weights, log=False, sigma2=sigma2)
     return cost[0]
 
 
 def costjac(params):
     """jacobian of cost function w/r to parameters alpha_xx and tau_xx"""
-    alphas, taus = params[:nTR], params[nTR:]
+    alphas, TRs = params[:nTR], params[nTR:]
     # simulate sequence
     jac, hes, num = epg.simulate(
-        sequence(alphas, taus, deriv=True), 
+        sequence(alphas, TRs, jac=True), 
         probe=[Jac, Hes, Num], 
         # callback=pruner, 
         max_nstate=nstate,
@@ -127,7 +139,8 @@ def costjac(params):
         np.moveaxis(jac, -1, 0), 
         np.moveaxis(hes, -1, 0),
         W=weights,
-        log=True,
+        sigma2=sigma2,
+        log=False,
     )
     niter = len(iterations)
     elaps = time.time() - tic
@@ -141,7 +154,10 @@ def costjac(params):
 
 def constraint_function(params):
     """constraint function: |alpha_i - alpha_i+1| < 1"""
-    return (1 - np.max(np.abs(np.diff(params[:nTR])))) * 10
+    # return (1 - np.max(np.abs(np.diff(params[:nTR])))) * 10
+    diff = np.diff(params, prepend=params[0])
+    diff[nTR:] = 0
+    return 1 - np.abs(diff)
 
 
 # initial FA between 10 and 60
@@ -152,7 +168,8 @@ for i in range(nTR//nFA + 1):
     FA = np.sin(np.arange(1, 1 + nFA) * np.pi / nFA) * (60 - 10) + 10
     FA[-10:] = 0
     init_FA.extend(FA.tolist())
-init_FA = init_FA[:nTR]
+init_FA = np.clip(init_FA[:nTR], 10, 60)
+
 
 # initial TR: Perlin noise 11.5 to 14.5 
 def smoothstep(x):
@@ -178,7 +195,7 @@ config = {
     "bounds": bounds,
     "constraints": [{"type": "ineq", "fun": constraint_function}],
     "callback": callback,
-    "options": {"disp": True, "ftol": 1e-7, "maxiter": 250},
+    "options": {"disp": True, "ftol": 1e-6, "maxiter": 250},
 }
 
 # optimize
@@ -192,7 +209,7 @@ tic = time.time()
 res = optimize.minimize(costfun, init, **config)
 
 duration = time.time() - tic
-print(f"Results: alphas={res.x[:nTR]}, taus={res.x[nTR:]}")
+print(f"Results: alphas={res.x[:nTR]}, TRs={res.x[nTR:]}")
 print(f"Optimization time: {duration/60:.1f} min")
 
 
@@ -200,8 +217,8 @@ print(f"Optimization time: {duration/60:.1f} min")
 # plot
 
 fig, axes = plt.subplots(nrows=2, num="mrf-optim", sharex=True)
-initconst = constraint_function(init)
-resconst = constraint_function(res.x)
+initconst = constraint_function(init).sum()
+resconst = constraint_function(res.x).sum()
 
 plt.sca(axes.flat[0])
 plt.plot(init[:nTR], label=f"initial (constraint cost: {initconst:.2f})")
@@ -210,14 +227,16 @@ plt.legend()
 plt.title("Flip angle values")
 plt.ylabel("flip angle (degree)")
 plt.ylim(0, 70)
+plt.grid()
 plt.sca(axes.flat[1])
 plt.plot(init[nTR:], label="initial")
 plt.plot(res.x[nTR:], label="optimized")
 plt.legend()
-plt.title("TE values")
-plt.ylabel("TE (ms)")
+plt.title("TR values")
+plt.ylabel("TR (ms)")
 plt.xlabel("echo index")
 plt.ylim(10, 17)
+plt.grid()
 plt.tight_layout()
 
 
@@ -226,8 +245,8 @@ sig0 = signal(init)
 crb0 = costfun(init)
 sig1 = signal(res.x)
 crb1 = costfun(res.x)
-plt.plot(np.abs(sig0[:, 0]), label=f"initial (log10(CRB): {crb0[0]:0.2f})")
-plt.plot(np.abs(sig1[:, 0]), label=f"optimized (log10(CRB): {crb1[0]:0.2f})")
+plt.plot(np.abs(sig0[:, 0]), label=f"initial (log10(CRB): {crb0:0.2f})")
+plt.plot(np.abs(sig1[:, 0]), label=f"optimized (log10(CRB): {crb1:0.2f})")
 plt.xlabel("echo index")
 plt.ylabel("signal")
 plt.title(f"Fingerprint for T1={T1}ms, T2={T2}ms")
@@ -241,7 +260,7 @@ jacs = [
     epg.simulate(sequence(params[:nTR], params[nTR:]), probe=Jac, max_nstate=10)[..., 0]
     for params in iterations
 ]
-crb_tot = stats.crlb(jacs, W=weights, log=False)
+crb_tot = stats.crlb(jacs, W=weights, log=False, sigma2=sigma2)
 crb_mag, crb_T1, crb_T2 = stats.crlb_split(jacs, W=weights, log=False)
 plt.plot(crb_mag, ":", label="CRB magnitude")
 plt.plot(crb_T1, ":", label="CRB T1")
@@ -249,7 +268,7 @@ plt.plot(crb_T2, ":", label="CRB T2")
 plt.plot(crb_tot, label="CRB total")
 plt.title("Evolution of CRB during optimization")
 plt.xlabel("Iteration index")
-plt.ylabel("log10(CRB)")
+plt.ylabel("CRB")
 plt.legend()
 plt.tight_layout()
 
