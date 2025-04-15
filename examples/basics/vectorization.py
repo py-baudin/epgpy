ca9ec9b2b8c88210bace604fa9e35c1dcd3bde15
Pierre-Yves Baudin <py-baudin@institut-myologie.org>
2025-04-15 14:19:25 +0200

    examples: vectorization slight improvmt

--- examples/basics/vectorization.py
+++ examples/basics/vectorization.py
@@ -25,18 +25,20 @@ assert signal.shape == (necho, 5, 3)
 
 # reduce with Adc
 
-# signal is reduced on selected axis/axes
+# signal is reduced (summed) on selected axis/axes
 adc = epg.Adc(reduce=0)
 seq = [exc] + [[rlx, spl, inv, spl, rlx, adc]] * 20
 signal_reduced = epg.simulate(seq)
 assert signal_reduced.shape == (20, 3)
+assert np.allclose(signal_reduced, signal.sum(axis=1))
 
 adc = epg.Adc(reduce=(0, 1))
 seq = [exc] + [[rlx, spl, inv, spl, rlx, adc]] * 20
 signal_reduced = epg.simulate(seq)
 assert signal_reduced.shape == (20,)
+assert np.allclose(signal_reduced, signal.sum(axis=(1, 2)))
 
-# weights (w/ automatic reduce)
+# set weights (w/ automatic reduce)
 weights = [i / 10 for i in range(5)]
 adc = epg.Adc(weights=weights)
 seq = [exc] + [[rlx, spl, inv, spl, rlx, adc]] * 20
@@ -44,9 +46,7 @@ signal_weights = epg.simulate(seq)
 assert signal_weights.shape == (20, 3)
 assert np.allclose(signal_weights, np.vecdot(weights, signal, axes=[0, 1]))
 
-
-# weigths with operator PD
-
+# set weigths with operator PD
 pd = epg.PD(weights)
 adc = epg.Adc(reduce=0)
 seq = [pd, exc] + [[rlx, spl, inv, spl, rlx, adc]] * 20
